68dd02160973daeb38128108502c7d5da324c986
Flapus22 <72880003+Flapus22@users.noreply.github.com>
2024-01-17 09:23:26 +0100

    Create User and DBContext for DataProject

---
+++ ZP_Filapek_Felkel_Makowiejczuk/Data/DBContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data;
+
+public class DBContext: DbContext
+{
+    public DBContext()
+    {
+
+    }
+
+    public DBContext(DbContextOptions<DBContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=ShoolDataBase;User ID=sa;Password=yourStrong(!)Password;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Email).IsRequired();
+            entity.Property(e => e.FirstName).IsRequired();
+        });
+
+    }
+
+    public DbSet<User> Users { get; internal set; }
+}
