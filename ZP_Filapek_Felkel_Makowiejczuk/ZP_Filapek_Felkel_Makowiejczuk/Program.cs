9e55f261a804cc0a52662b702b473bea6ae2da6d
Flapus22 <72880003+Flapus22@users.noreply.github.com>
2024-01-19 10:17:37 +0100

    *Update Usings in Porgram.cs

--- ZP_Filapek_Felkel_Makowiejczuk/ZP_Filapek_Felkel_Makowiejczuk/Program.cs
+++ ZP_Filapek_Felkel_Makowiejczuk/ZP_Filapek_Felkel_Makowiejczuk/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 using ZP_Filapek_Felkel_Makowiejczuk.Interface.Authentication;
+using ZP_Filapek_Felkel_Makowiejczuk.Model.Authentication;
 using ZP_Filapek_Felkel_Makowiejczuk.Services.Authentication;
 
 
