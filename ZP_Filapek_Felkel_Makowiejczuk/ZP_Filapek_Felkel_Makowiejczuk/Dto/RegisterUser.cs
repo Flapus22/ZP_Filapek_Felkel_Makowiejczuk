3b89dcec8809a87fdeab27ce1d969f5f0c60924a
Michał Makowiejczuk <makowiejczuk.michal@gmail.com>
2024-01-23 19:47:14 +0100

    Adding properties in the RegisterUser class

--- ZP_Filapek_Felkel_Makowiejczuk/ZP_Filapek_Felkel_Makowiejczuk/Dto/RegisterUser.cs
+++ ZP_Filapek_Felkel_Makowiejczuk/ZP_Filapek_Felkel_Makowiejczuk/Dto/RegisterUser.cs
@@ -1,4 +1,5 @@
 ﻿namespace ZP_Filapek_Felkel_Makowiejczuk.Dto;
+using Data.Model;
 
 public class RegisterUser
 {
@@ -8,4 +9,11 @@ public class RegisterUser
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime? DateOfBirth { get; set; }
+    public string City { get; set; }
+    public string? District { get; set; }
+    public string Street { get; set; }
+    public string PostalCode { get; set; }
+    public string HouseNumber { get; set; }
+    public UserType UserType { get; set; }
+    
 }
